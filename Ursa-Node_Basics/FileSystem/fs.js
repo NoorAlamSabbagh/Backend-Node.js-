ea3b17c0ce42292516df195606dd72977f59355c
Noor Alam <alamrangrej787@gmail.com>
2023-02-24 19:26:30 +0530

    Lec6Feb22

--- Ursa-Node_Basics/FileSystem/fs.js
+++ Ursa-Node_Basics/FileSystem/fs.js
@@ -1,11 +1,13 @@
+//<===========================================================>//
+// Lec5Feb21: Using Core Module, OS Module
 // File system CRUD
 
 const fs = require("fs");
 // const { clearScreenDown } = require("readline");
 
-/*
-// const fsPromise = require('fs/promises');
+// const fsPromise = require('fs/promises');//(most latest way of handelling promises)
 
+/*
 //
 // working with Directories
 //below code create new directory
@@ -57,6 +59,7 @@ console.log('end');
 // })
 
 //
+/*
 fs.readdir('Filesystem', (e, files) => {
     if(e){
         console.log(`error occured: ${e.message}`);
@@ -65,3 +68,123 @@ fs.readdir('Filesystem', (e, files) => {
         console.log(files);
     }
 })
+*/
+
+//
+/*
+fs.rmdir('Filesystem/my', {recursive: true}, (err) => {
+    if(err){
+        console.log(`error occured: ${err.message}`);
+    } else{
+        console.log('deleted succesfully');
+    }
+})
+*/
+//don with directory
+//<===========================================================>//
+
+
+
+
+//<===========================================================>//
+// Lec6Feb22: Creating and working files, Events module, Emitting Events
+
+//
+// CRUD on files
+/*
+// if file is not there then it will create one, and then write the content
+// if file is there already then it will overwrite the content in that file
+fs.writeFile('Filesystem/myFile.txt', 'hi this is Abhinav', (err) => {
+    if(err){
+      console.log(`error occured: ${err.message}`);
+    }
+  })
+  */ 
+
+  /*
+  // if file is not there then it will create one, and then write the content
+  // if file is there already then it will append the content in that file
+  fs.appendFile('Filesystem/File.txt', '\nHi this line no 1', (err) => {
+    if(err){
+      console.log(`error occured: ${err.message}`);
+    }else{
+      console.log('file appended successfully');
+    }
+  })
+*/
+//   fs.open('Filesystem/File.txt', 'r') 
+
+/*
+// open
+  fs.open('Filesystem/File.txt', 'r', (err, file) => {
+    if(err){
+      console.log(`error occured: ${err.message}`);
+    }else{
+      console.log(file);
+      console.log('file appended successfully');
+    }
+  })
+*/
+
+//
+/*
+  fs.readFile('Filesystem/File.txt', 'utf-8', (err, file) => {
+    if(err){
+      console.log(`error occured: ${err.message}`);
+    }else{
+      // console.log(file+'');
+      console.log(file);
+      console.log('file reading successfully');
+    }
+  })
+  */
+
+  
+  //to delete file
+  /*
+  fs.unlink('Filesystem/myFile.js', (err) => {
+    if(err){
+      console.log(`error occured: ${err.message}`);
+    } else{
+      console.log('file deleted successfully');
+    }
+  })
+  */
+
+  //same code as above, now using async method
+  /* 
+  const fsPromise = require('fs/promises');//(most latest way of handelling promises)
+  const deleteFile = async() => {
+    await fsPromise.unlink('Filesystem/myFile.txt');
+  }
+  
+  deleteFile(); 
+  */
+  
+  /*
+ (function(){
+  
+  })()
+*/
+
+
+
+
+
+//callback
+/*
+fetch('https://myapi.com')
+.then((data) => {
+})
+
+.catch(() => {
+
+})
+*/
+
+//
+// const callmyApi = async() => {
+//   await fetch('https://myapi.com')
+// }
+
+//<===========================================================>

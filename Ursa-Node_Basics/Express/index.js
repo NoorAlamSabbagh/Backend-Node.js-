c01d13ca12491af91e7880f4534889306d65408e
Noor Alam <alamrangrej787@gmail.com>
2023-03-01 01:22:13 +0530

    Lec9Feb27

--- Ursa-Node_Basics/Express/index.js
+++ Ursa-Node_Basics/Express/index.js
@@ -12,7 +12,7 @@
 
 // app.get('/login',(req, res)=>{
 //     console.log('///login endPoint');
-//     res.end('<h1>login page....</h1>')
+//     res.send('<h1>login page....</h1>')
 // })
 
 // app.get('/data',(req, res)=>{
@@ -26,3 +26,198 @@
 // })
 
 //<===========================================================>//
+
+
+//<===========================================================>//
+// Lec9Feb27: Introducing Nodemon, Route Parameters, Handling Https
+
+//
+// const express = require('express');
+
+
+
+// const app = express();
+
+// app.get('/',(req, res)=>{
+//     console.log('///endPoint');
+//     res.send('<h2>Default page using express</h2>');
+// })
+
+// app.get('/login',(req, res)=>{
+//     console.log("method", req.method);
+//     console.log("url", req.url);
+//     console.log("path", req.path);
+//     console.log("headers", req.headers);
+//     console.log("query", req.query);
+//     console.log(req);
+//     res.send('<h1>login page....</h1>')
+// })
+
+// app.get('/data',(req, res)=>{
+//     console.log('/// data endPoint');
+//     res.send({name:'Abhinav'});
+// })
+
+// app.get('*',(req, res)=>{
+//     console.log('not found page');
+//     res.send('<h3>Page not found !!</h3>');
+// })
+
+// const PORT = 5500;
+// app.listen(PORT, () =>{
+//     console.log(`express running at ${PORT}`);
+// })
+
+
+
+////////////////////////////////////
+/*
+const express = require("express");
+
+const app = express();
+
+// to get the data from client where body is in form of json
+app.use(express.json());
+
+// to get the data from a form which will have the format as urlencoded
+// app.use(express.urlencoded());
+
+const product = [
+  {
+    id: 1,
+    name: "iphone 13",
+    category: "Mobile",
+    price: 50000,
+    color: "black",
+  },
+  {
+    id: 2,
+    name: "galaxy",
+    category: "Mobile",
+    price: 20000,
+    color: "white",
+  },
+  {
+    id: 3,
+    name: "fridge",
+    category: "appliances",
+    price: 20000,
+    color: "green",
+  },
+  {
+    id: 4,
+    name: "cooler",
+    category: "appliances",
+    price: 25000,
+    color: "gray",
+  },
+];
+
+app.get("/", (req, res) => {//browser se get request jata ha
+    console.log("get endpoint");
+    res.send("<h2>Default page GET</h2>");
+  });
+  
+  app.post("/", (req, res) =>{
+      console.log("post endpoint");
+      res.send("<h2>Default page POST</h2>");
+  })
+  
+  app.get("/login", (req, res) => {
+    console.log("method", req.method);
+    console.log("url", req.url);
+    console.log("path", req.path);
+    console.log("headers", req.headers);
+    console.log("query", req.query);
+    console.log(req);
+    res.send("<h1>login page..</h1>");
+  });
+  
+  app.get("/data", (req, res) => {
+    console.log("data endpoint");
+    res.send({ name: "Abhinav" });
+  });
+
+  //to get products
+//   app.get("/products", (req, res) => {
+//     res.send(product);
+//   });
+   
+app.get("/products/:productId", (req, res) => {
+    console.log("params", req.params);
+    const productItem = product.find((prd) => prd.id === Number(req.params.productId));
+      console.log('productItem', productItem);
+      if(productItem){
+          res.send(productItem)
+      }else{
+          res.send({})
+      }
+  });
+
+  app.get("/products", (req, res) => {
+    console.log('query', req.query);
+    const {category, price} = req.query;//destructuring
+    console.log('category', category);
+    console.log('price', price);
+
+    if(category){
+        const productList = product.filter((prd) => prd.category === category);
+        res.send(productList);
+    }
+    if(price){
+        const productList = product.filter((prd) => prd.price === Number(price));
+        res.send(productList);
+    }
+    // refactor this logic
+    res.send(product);
+});
+
+  app.get('*',(req, res)=>{
+    console.log('not found page');
+    res.send('<h3>Page not found !!</h3>');
+})
+
+const PORT = 5500;
+app.listen(PORT, () =>{
+    console.log(`express running at ${PORT}`);
+})
+*/
+///////////////////////////////
+
+/*
+// http://localhost:5500/cart
+// http://localhost:5500/coments
+// http://localhost:5500/cart
+
+
+
+// try this
+// http://localhost:5500/products?price=20000&category=mobile
+// result should be 
+// {id: 2,
+//     name: "galaxy",
+//     category: "Mobile",
+//     price: 20000,
+//     color: "white
+// }
+
+// http://localhost:5500/products
+// should return all products
+
+
+//
+// const userInfo = {
+//     username: 'abhinav',
+//     password: '12345'
+// }
+
+// fetch('https://myapi.com', {
+//     method: POST,
+//     body: JSON.stringify(userInfo),
+// })
+
+// axios.post('https://myapi.com', userInfo)
+*/
+
+
+//<==================End Of Lec9Feb27(line 31 se  222 tak isi index.js me code likha ha is lec me)=========================================>//

f535fa556892dafed459334b5721436f774fcef2
Noor Alam <alamrangrej787@gmail.com>
2023-05-03 20:44:27 +0530

    Revised_Lec7

--- Ursa-Node_Basics/Node_Basics/httpModule/http.js
+++ Ursa-Node_Basics/Node_Basics/httpModule/http.js
@@ -1,24 +1,65 @@
-//<===========================================================>//
+//<======================= Lec7Feb23: after studying eventjs folder ====================================>//
 // Lec7Feb23: Creating and working files, Events module, Emitting Events
-// const http = require('http');
-// const server = http.createServer((req, res) => {
-//     console.log('req received');
-// })
 
-// const PORT = 8000
-// server.listen(PORT, () => {
-//     console.log(`server is running at ${PORT}`);
-// })
+/*const http = require('http');
+const server = http.createServer((req, res) => {
+    console.log('req received');
+})
+
+const PORT = 8000
+server.listen(PORT, () => {
+    console.log(`server is running at ${PORT}`);
+})
+*/
 
 //
 /*
 const http = require('http');
 const server = http.createServer((req, res) => {
-    // console.log('req received');
-    // console.log('req is', req);
-    console.log('headers', req.headers);
-    console.log('url',req.url)
+    console.log('req received');
+    res.end("sending Respnse")
+})
+
+const PORT = 8000
+server.listen(PORT, () => {
+    console.log(`server is running at ${PORT}`);
+})
+*/
+
+//
+/*
+const http = require('http');
+const server = http.createServer((req, res) => {
+    console.log('req received');
+    res.end("sending Respnse")
+})
+
+//server internally implements event module
+server.on('error', (req,res)=>{
+    console.log('error inside server')
+})
+server.emit('error');
+
+const PORT = 8000
+server.listen(PORT, () => {
+    console.log(`server is running at ${PORT}`);
+})
+*/
+
+
+//
+/*
+const http = require('http');
+const server = http.createServer((req, res) => {
+    console.log('req is', req);
+    // console.log('headers', req.headers);//header ka pura aayega
+    console.log('url',req.url)//route bana dega
     res.end('sending response');
+})
+
+const PORT = 8000;
+server.listen(PORT, ()=>{
+    console.log(`server is running at ${PORT}`);
 })
 */
 
@@ -30,6 +71,7 @@ function calculate(a,b){
 const res = calculate(10,40)
 */
 
+//broweser pe post request kiya
 // fetch('http:localhost/3000', {
 //     method: "POST"
 // })
@@ -41,29 +83,29 @@ const res = calculate(10,40)
 
 
 //
-// const http = require('http');
-// const server = http.createServer((req, res) => {
-//     console.log('url', req.url);
-//     if(req.url === '/'){
-//         res.write("{name: 'Abhinav'}");
-//         // res.end('<h2>default page</h2>')
-//     }else if(req.url === '/home'){
-//         res.end('<h2>Home Page</h2>')
-//     }else if(req.url === '/login'){
-//         res.end('<h2>Login Page</h2>')
-//     }else{
-//         res.end('sending Response');
-//     }
-//     // res.end()
-//     res.end('sending Response');
-// })
+/*
+const http = require('http');
+const server = http.createServer((req, res) => {
+    console.log('url', req.url);
+    if(req.url === '/'){
+        res.write("{name: 'Abhinav'}");
+        // res.end('<h2>default page</h2>')
+    }else if(req.url === '/home'){
+        res.end('<h2>Home Page</h2>')
+    }else if(req.url === '/login'){
+        res.end('<h2>Login Page</h2>')
+    }else{
+        res.end('sending Response');
+    }
+    res.end()
+    // res.end('sending Response');
+})
 
-
-
-// const PORT = 8000
-// server.listen(PORT, () => {
-//     console.log(`server is running at ${PORT}`);
-// })
+const PORT = 8000
+server.listen(PORT, () => {
+    console.log(`server is running at ${PORT}`);
+})
+*/
 
 /*
     // console.log('headers', req.headers);
